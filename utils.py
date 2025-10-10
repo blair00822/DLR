e7e8ffe9ea9505f6f6e49e56e3616a14daceb949
blair00822 <166817559+blair00822@users.noreply.github.com>
2025-10-10 17:28:37 +0800

    Update and rename new_train.py to utils.py

---
+++ utils.py
@@ -0,0 +1,32 @@
+import torch
+from torch import optim
+from torch.utils.data import DataLoader
+import os, csv, random, logging, time
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.backends.cudnn.deterministic = False
+        
+def get_logger(filename, verbosity=1, name=None):
+    level_dict = {0: logging.DEBUG, 1: logging.INFO, 2: logging.WARNING}
+    formatter = logging.Formatter(
+        "[%(asctime)s][%(filename)s][line:%(lineno)d][%(levelname)s] %(message)s"
+    )
+    logger = logging.getLogger(name)
+    logger.setLevel(level_dict[verbosity])
+    fh = logging.FileHandler(filename, "a")
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+    return logger            
+    
+    
